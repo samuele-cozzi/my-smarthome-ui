ea566a51c00c4aa1075ebe1277d8f556dd921719
samuele-cozzi <samuele.cozzi@gmail.com>
2022-09-22 21:08:45 +0000

    add global using

---
+++ api/GlobalUsing.cs
@@ -0,0 +1,18 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Text.Json;
+global using System.Threading;
+global using System.Threading.Tasks;
+
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Options;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+global using System.Net.Http;
+global using System.Net.Http.Headers;
+//global using Microsoft.Azure.Devices;
+
+//global using home.api.models;
