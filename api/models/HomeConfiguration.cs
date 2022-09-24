39bc93bfe83a9d4ad24a71f8c61cf68ec7b2dd84
samuele-cozzi <samuele.cozzi@gmail.com>
2022-09-24 19:18:00 +0000

    change model

--- api/models/HomeConfiguration.cs
+++ api/models/HomeConfiguration.cs
@@ -3,7 +3,12 @@ namespace home.api.models;
 public class HomeConfiguration {
     public int TargetHumidity { get; set; }
     public int TargetTemperature { get; set; }
+    public int TemperatureTolerance { get; set; }
     public int IntervalThermostat { get; set; }
     public bool Enabled { get; set; }
+    public string Mode { get; set; }
+    public string AirMode { get; set; }
+    public int AirFan { get; set; }
+    public int AirTemperature { get; set; }
     public List<HomeRoom> Rooms { get; set; }
 }
