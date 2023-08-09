cf21fb9b316b6d12f9aaa472224a9730e839f5fd
samuele-cozzi <samuele.cozzi@gmail.com>
2023-08-09 21:59:41 +0000

    add oneSignal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OneSignal } from 'onesignal-ngx';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ui-ac-controller';
+
+  constructor(private oneSignal: OneSignal) {
+    this.oneSignal.init({
+      appId: "17d959dc-69a8-48d7-be88-8cae68dd2ea2",
+    });
+  }
 }
